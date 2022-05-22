edce1e26186493b5e89adee1bccd271966f4fe5b
Sebastian <sebastianvel1425@gmail.com>
2022-05-22 14:41:29 -0500

    Creacion de la zona donde se desarrolla el juego

--- js/contenido-dinamico.js
+++ js/contenido-dinamico.js
@@ -1,21 +1,29 @@
+var btnIniciar = document.querySelector("#btnIni");
 var btnAgregar = document.querySelector("#btnAgr");
-//var btnIniciar = document.querySelector("#btnIni");
+var btnCancelar = document.querySelector("#btnCancelar");
+var btnDesistir = document.querySelector("#btnDesistir");
+
 //var frmJuego = document.querySelector("");
 const frmIniciar = document.querySelector("#menu");
 const frmAgregar = document.querySelector("#agrPalabra");
+const frmJuego = document.querySelector("#secJuego");
 
+btnIniciar.addEventListener("click", function(){
+    gestorContenido(frmJuego, frmIniciar);
+});
 
 btnAgregar.addEventListener("click", function(){            
     gestorContenido(frmAgregar,frmIniciar);
-    const btnCancelar = document.querySelector("btnCancelar");
+    
 });
-
-
 
 btnCancelar.addEventListener("click", function(){
     gestorContenido(frmIniciar,frmAgregar);
 });
 
+btnDesistir.addEventListener("click", function(){
+    gestorContenido(frmIniciar,frmJuego);
+});
 function gestorContenido(mostrar, quitar){    
     quitar.classList.add("ocultar");
     mostrar.classList.remove("ocultar");
---
+++ js/graficar.js
@@ -0,0 +1,4 @@
+var canvAhorcado = document.querySelector("#canvahorcado");
+var pincel = canvAhorcado.getContext("2d");
+pincel.fillStyle = "grey"; 
+pincel.fillRect(0,0,500,400);
